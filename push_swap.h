80a7b8be549681a66e66bba61bfcf892c30d0857
DKOK01 <aymansadik2005@gmail.com>
2025-02-26 11:26:36 +0000

    .

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aysadeq <aysadeq@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 11:06:51 by aysadeq           #+#    #+#             */
+/*   Updated: 2025/02/26 11:16:02 by aysadeq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# in
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}	t_node;
+
+#endif
