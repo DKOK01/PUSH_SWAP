b6fa8bec7271128b3c741f2183a31d8cfe5f4f8b
DKOK01 <aymansadik2005@gmail.com>
2025-03-14 13:45:21 +0000

    .

--- checker.sh
+++ checker.sh
@@ -1,9 +1,9 @@
 while true; do
-    args=$(shuf -i 1-9999 -n 100 | tr '\n' ' ')
+    args=$(shuf -i 1-9999 -n 500 | tr '\n' ' ')
     i=$(./push_swap $args | wc -l)
     status=$(./push_swap $args | ./checker_linux $args)
 
-    if [[ "$i" -gt 700 || "$status" == "KO" ]]; then
+    if [[ "$i" -gt 5500 || "$status" == "KO" ]]; then
         printf "\e[31mError: %d instructions\e[0m\n" "$i"
         printf "\e[31mArgs: %s\e[0m\n" "$args"
         break
